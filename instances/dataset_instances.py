81e7594e5b8ba8d7131773c1a49d3610eb12f840
Diego Souza <souza.claudiodiego@gmail.com>
2023-11-17 20:38:32 -0300

    ngsim resolution

--- instances/dataset_instances.py
+++ instances/dataset_instances.py
@@ -7,7 +7,7 @@ NGSIM = Dataset(name='ngsim',
                 lat_y_max=2235.252,
                 lon_x_min=0.0,
                 lon_x_max=75.313,
-                resolution=(200, 40),
+                resolution=(200, 20),
                 indices=LatYLonXTimeIndices(1, 0, 3),
                 is_lat_lon=False)
 
