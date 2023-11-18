1480c5cc6132cd4ef178bd3932b0c6c5d204504c
Diego Souza <souza.claudiodiego@gmail.com>
2023-11-17 22:12:34 -0300

    wip fcae props

--- instances/fcae_properties_instances.py
+++ instances/fcae_properties_instances.py
@@ -1,5 +1,5 @@
 from helpers.types_helper import FCAEProperties
 
-AE_PROPERTIES = FCAEProperties(input_shape=(4, 12, 1), encode_layers=[128, 64], encode_activation='relu',
-                               decode_activation='linear', kernel_size=(5, 5), encode_strides=[2, 2], padding='same',
-                               latent_space=12, learning_rate=0.0001)
+AE_PROPERTIES = FCAEProperties(input_shape=(4, 12, 1), encode_layers=[64, 32], encode_activation='relu',
+                               decode_activation='relu', kernel_size=(2, 2), encode_strides=[2, 2], padding='same',
+                               latent_space=10, learning_rate=0.0005)
