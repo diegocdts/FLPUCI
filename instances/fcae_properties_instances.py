66dc111074459b5f0f49df27360e32681543c67b
Diego Souza <souza.claudiodiego@gmail.com>
2023-11-22 15:29:19 -0300

    ngsim AE_PROPERTIES

--- instances/fcae_properties_instances.py
+++ instances/fcae_properties_instances.py
@@ -1,5 +1,11 @@
 from helpers.types_helper import FCAEProperties
 
-AE_PROPERTIES = FCAEProperties(input_shape=(4, 12, 1), encode_layers=[64, 32], encode_activation='relu',
-                               decode_activation='relu', kernel_size=(2, 2), encode_strides=[2, 2], padding='same',
-                               latent_space=10, learning_rate=0.001)
+AE_PROPERTIES = FCAEProperties(input_shape=(4, 56, 1),
+                               encode_layers=[16, 8],
+                               encode_activation='relu',
+                               decode_activation='relu',
+                               kernel_size=(5, 5),
+                               encode_strides=[2, 2],
+                               padding='same',
+                               latent_space=10,
+                               learning_rate=0.001)
