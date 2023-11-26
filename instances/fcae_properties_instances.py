bd02473d9394dd5886f10867b4b8d75edfbff91c
Diego Souza <souza.claudiodiego@gmail.com>
2023-11-26 12:57:36 -0300

    SF main

--- instances/fcae_properties_instances.py
+++ instances/fcae_properties_instances.py
@@ -1,11 +1,21 @@
 from helpers.types_helper import FCAEProperties
 
-AE_PROPERTIES = FCAEProperties(input_shape=(4, 56, 1),
-                               encode_layers=[16, 8],
-                               encode_activation='relu',
-                               decode_activation='relu',
-                               kernel_size=(5, 5),
-                               encode_strides=[2, 2],
-                               padding='same',
-                               latent_space=10,
-                               learning_rate=0.001)
+AE_PROPERTIES_NGSIM = FCAEProperties(input_shape=(4, 56, 1),
+                                     encode_layers=[16, 8],
+                                     encode_activation='relu',
+                                     decode_activation='relu',
+                                     kernel_size=(5, 5),
+                                     encode_strides=[2, 2],
+                                     padding='same',
+                                     latent_space=10,
+                                     learning_rate=0.001)
+
+AE_PROPERTIES_SF = FCAEProperties(input_shape=(40, 40, 1),
+                                  encode_layers=[128, 64, 32],
+                                  encode_activation='relu',
+                                  decode_activation='linear',
+                                  kernel_size=(3, 3),
+                                  encode_strides=[2, 2, 2],
+                                  padding='same',
+                                  latent_space=10,
+                                  learning_rate=0.0005)
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import warnings
 import nest_asyncio
 
-from instances.dataset_instances import NGSIM
-from instances.fcae_properties_instances import AE_PROPERTIES
+from instances.dataset_instances import SF
+from instances.fcae_properties_instances import AE_PROPERTIES_SF
 from instances.sample_selection_instances import SLI_3
 from instances.training_instances import FEDERATED_TRAINING_1, CENTRALIZED_TRAINING_1
 from instances.validation_instances import NEXT
@@ -13,20 +13,20 @@ from communities_identification import cen_communities_identification, fed_commu
 warnings.filterwarnings("ignore")
 nest_asyncio.apply()
 
-fed_communities_identification(dataset=NGSIM,
-                               ae_properties=AE_PROPERTIES,
+fed_communities_identification(dataset=SF,
+                               ae_properties=AE_PROPERTIES_SF,
                                training_parameters=FEDERATED_TRAINING_1,
                                sli_selection=SLI_3,
                                first_interval=0,
-                               last_interval=10,
+                               last_interval=11,
                                validate_at=NEXT,
                                acc_run=True)
-cen_communities_identification(dataset=NGSIM,
-                               sample_handler=SampleHandler(NGSIM),
-                               ae_properties=AE_PROPERTIES,
+cen_communities_identification(dataset=SF,
+                               sample_handler=SampleHandler(SF),
+                               ae_properties=AE_PROPERTIES_SF,
                                training_parameters=CENTRALIZED_TRAINING_1,
                                sli_selection=SLI_3,
                                first_interval=0,
-                               last_interval=10,
+                               last_interval=11,
                                validate_at=NEXT,
                                acc_run=True)
